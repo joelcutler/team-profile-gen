e9d7d83ffc2cbb438be7ae8d7bce1a9fa01b4ca2
Joel C <joel7921111@yahoo.com>
2022-04-02 16:55:15 -0600

    created html file to write the literal for generateHTML, it's called htmlLiteral

--- src/generateHtml.js
+++ src/generateHtml.js
@@ -1,16 +1,27 @@
 const generateHTML = (teamRoster) => {
   return `
-<!DOCTYPE html>
+  <!DOCTYPE html>
 
-<head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" charset="UTF-8" />
+  <head>
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1.0"
+      charset="UTF-8"
+    />
     <title>Team Profile Generator</title>
-    <link rel="stylesheet" href="./assets/css/style.css" />
-</head>
-
-<body>
-<p> hardcode teamroster card here </p>
-</body>`;
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css"
+    />
+    <link rel="stylesheet" href="./dist/style.css" />
+  </head>
+  
+  <body>
+    <header class="has-text-centered">
+      <h1 class="is-size-1">The Squad!</h1>
+    </header>
+  </body>
+  `;
 };
 
 module.exports = generateHTML;
